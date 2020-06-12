d5c6ec1d33c8f05a3c02e678090628ed905957ae
David Shorthouse <davidpshorthouse@gmail.com>
2020-06-12 16:21:33 -0400

    Rework all from previous repo

---
+++ README.md
@@ -0,0 +1,25 @@
+bionomia-chrome
+======================
+
+Adjust Global Biodiversity Information Facility (GBIF) occurrence pages, eg [https://www.gbif.org/occurrence/477976412][1] to include links to ORCID accounts for those who collected or identified the specimen and links to published works that used the specimen data. Also adjust GBIF dataset pages eg [https://www.gbif.org/dataset/275319e1-f91c-406f-b239-62cb9d4185cb][2] to include a button linking to a page that shows a list of people who collected and identified specimens included in the dataset under view.
+
+Support
+-------
+
+Bug reports can be filed at [https://github.com/bionomia/bionomia-chrome/issues][4].
+
+License
+-------
+
+bionomia-chrome is released under the [MIT license][3].
+
+Copyright
+---------
+
+Copyright (c) [David P. Shorthouse][5]
+
+[1]: https://www.gbif.org/occurrence/477976412
+[2]: https://www.gbif.org/dataset/275319e1-f91c-406f-b239-62cb9d4185cb
+[3]: http://www.opensource.org/licenses/MIT
+[4]: https://github.com/bionomia/bionomia-chrome/issues
+[5]: https://github.com/dshorthouse
---
+++ js/background.js
@@ -0,0 +1,81 @@
+/*global chrome, Object, alert */
+var BionomiaBackground = (function($, window, document) {
+
+  "use strict";
+
+  var _private = {
+
+    vars: {
+      gbifID: 0
+    },
+
+    receiveMessages: function() {
+      var self = this;
+
+      chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+        switch(request.method) {
+
+          case 'bn_gbifID':
+            chrome.tabs.query({active : true, currentWindow : true}, function(tab) {
+              tab = tab[0];
+              $.ajax({
+                type: "GET",
+                url: "https://bionomia.net/occurrence/" + request.params.gbifID + ".json",
+                dataType: "json"
+              }).done(function(data) {
+                chrome.tabs.sendMessage(tab.id, { method : "bn_occurrence", params : { data: data } });
+                sendResponse({});
+                return true;
+              })
+              .fail(function(data) {
+                chrome.tabs.sendMessage(tab.id, { method : "bn_occurrence", params : { data: { message: "error"} } });
+                sendResponse({});
+                return true;
+              });
+            });
+          break;
+
+          case 'bn_gbifDatasetKey':
+            chrome.tabs.query({active : true, currentWindow : true}, function(tab) {
+              tab = tab[0];
+              $.ajax({
+                type: "GET",
+                url: "https://bionomia.net/dataset/" + request.params.gbifDatasetKey + ".json",
+                dataType: "json"
+              }).done(function(data) {
+                chrome.tabs.sendMessage(tab.id, { method : "bn_dataset", params : { data: data } });
+                sendResponse({});
+                return true;
+              })
+              .fail(function(data) {
+                chrome.tabs.sendMessage(tab.id, { method : "bn_dataset", params : { data: { message: "error"} } });
+                sendResponse({});
+                return true;
+              });
+            });
+          break;
+
+          default:
+            sendResponse({});
+            return true;
+        }
+        return true;
+      });
+    }
+
+  };
+
+  return {
+    init: function() {
+      _private.receiveMessages();
+    }
+  };
+
+}(jQuery, window, document));
+
+$(function() {
+  BionomiaBackground.init();
+  chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+    chrome.tabs.sendMessage(details.tabId, { method : "bn_flush" });
+  });
+});
